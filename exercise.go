3e42591a7e348dda3933f674ca261c4e582d5679
jirayu <jirayu.photisan@gmail.com>
2017-09-23 12:14:16 +0700

    factorial

--- exercise.go
+++ exercise.go
@@ -1,5 +1,13 @@
 package golang
 
-func stepup(a ,b int) (int, int){
-	return b , a + b
+func stepup(a, b int) (int, int) {
+	return b, a + b
+}
+
+func factorial(n int) int {
+	if n <= 1 {
+		return 1
+	}
+
+	return n * factorial(n-1)
 }
