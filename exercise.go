766ed3dae578638b13d83fe413c4d492c2bce5ca
jirayu <jirayu.photisan@gmail.com>
2017-09-23 15:17:49 +0700

    LettersCount

--- exercise.go
+++ exercise.go
@@ -1,37 +1,17 @@
 package golang
 
-func fibonacci(a int) []int {
-	//{\displaystyle F_{n}=F_{n-1}+F_{n-2},}
-	result := []int{}
+import (
+	"strings"
+)
 
-	for i := 0; i < a; i++ {
-		if i == 0 || i == 1 {
-			result = append(result, i)
-		} else {
-			result = append(result, result[i-1]+result[i-2])
-		}
+// LettersCount is function
+func LettersCount(sentenses string) map[string]int {
+	var m = map[string]int{}
+	split := strings.Split(sentenses, " ")
+
+	for _, v := range split {
+		m[v] = len(v)
 	}
 
-	return result
-}
-
-func concat(sorce, add []string) []string {
-
-	return append(sorce, add...)
-}
-
-func delFirst(a []string) []string {
-	return a[1:]
-}
-
-func delLast(a []string) []string {
-	return a[:len(a)-1]
-}
-
-func delSecond(a []string) []string {
-	first := make([]string, len(a))
-	first[0] = a[0]
-	last := a[2:]
-
-	return append(first, last...)
+	return m
 }
