32730ddfde3675a234062dd6097d55cbd533a7aa
jirayu <jirayu.photisan@gmail.com>
2017-09-24 11:50:29 +0700

    ex09

---
+++ exercise.go
@@ -0,0 +1,15 @@
+package golang
+
+func what(i interface{}) string {
+	switch i.(type) {
+	case int:
+		return "int"
+	case string:
+		return "string"
+	case bool:
+		return "bool"
+	default:
+		return ""
+	}
+
+}
